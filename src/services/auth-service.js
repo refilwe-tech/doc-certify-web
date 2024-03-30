468d8e236ec2caae92e8e6a30c58698f0d27ce0c
refilwe-m <refilwe.dev@gmail.com>
2024-03-30 12:15:47 +0200

    Register page and integration

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -7,12 +7,24 @@ const userUrls = {
   register: `${hostUrl}/register`,
 };
 
+const registerDTO = (user) => ({
+  first_name: user.firstName,
+  last_name: user.lastName,
+  username: user.username,
+  email: user.email,
+  role_id: 3,
+  password: user.password,
+  phone: user.phone ?? "",
+});
+
 const login = (user) => {
   return axios.post(userUrls.login, user).then((response) => response.data);
 };
 
 const register = (user) => {
-  return axios.post(userUrls.register, user).then((response) => response.data);
+  return axios
+    .post(userUrls.register, registerDTO(user))
+    .then((response) => response.data);
 };
 
 export default {
