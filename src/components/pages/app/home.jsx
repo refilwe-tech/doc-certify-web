daac8792cc83ff3b3a4bf6f9ab2b7d3a8a442bee
refilwe-m <refilwe.dev@gmail.com>
2024-03-30 10:39:36 +0200

    Clean up

--- src/components/pages/app/home.jsx
+++ src/components/pages/app/home.jsx
@@ -2,9 +2,12 @@ import { userStore } from "../../../reducers";
 
 export const HomePage = () => {
   const { user } = userStore();
+  const { username, email } = user;
   return (
     <section>
-      <h1 className="text-xl font-semibold">Hello, {user?.email ?? ""} 👋</h1>
+      <h1 className="text-xl font-semibold">
+        Hello, {(username || email) ?? ""} 👋
+      </h1>
     </section>
   );
 };
