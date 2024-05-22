879161a6933a09ffb97f0b925073fe98e6e07eeb
refilwe-m <refilwe.dev@gmail.com>
2024-05-23 00:10:54 +0200

    Certifier name shown

--- src/models/doc-model.js
+++ src/models/doc-model.js
@@ -2,14 +2,16 @@ import map from "lodash/map";
 import dayjs from "dayjs";
 
 export const docModel = (data) => {
+  console.log("Data", data);
   return {
     uploadDate: dayjs(data.upload_date).format("DD MMM YYYY HH:mm"),
     endDate: dayjs(data.end_date).format("DD/MM/YYYY"),
     dame: data.name,
     docType: data.document_type,
     status: data.status,
-    /* CertifiedFile: doc.certified_file,
-    OriginalFile: doc.original_file, */
+    certifier: data?.certifier
+      ? `${data?.certifier?.first_name + " " + data?.certifier?.last_name}`
+      : "",
     copyFile: data.copy_file,
     docID: data.document_id,
     clientID: data.client_id,
