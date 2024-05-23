4c9b644dbeae8a50c815645c39e2abf13c6d6b9f
refilwe-m <refilwe.dev@gmail.com>
2024-05-23 10:43:10 +0200

    removed logins

--- src/components/forms/login-form.jsx
+++ src/components/forms/login-form.jsx
@@ -14,9 +14,9 @@ export const LoginForm = () => {
   const { login } = userStore();
   const { handleSubmit, handleChange, values } = useFormik({
     initialValues: {
-      email: "johnw@mail.com",
-      password: "john123",
-      role: "Certifier",
+      email: "",
+      password: "",
+      role: "Certifyee",
     },
 
     onSubmit: (values) => {
